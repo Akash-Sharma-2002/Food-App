9f864d84e0c890d3da154ce7343acb72c0a76ef3
Akash-Sharma-2002 <akashsharma21221@gmail.com>
2025-08-25 00:33:51 +0530

    header Page Created

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Home.css";
+import Header from "../../components/Header/Header";
 const Home = () => {
-  return <div>Home</div>;
+  return (<div>
+    <Header />
+  </div>);
 };
 
 export default Home;

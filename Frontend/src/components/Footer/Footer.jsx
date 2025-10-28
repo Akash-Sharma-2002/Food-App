1515de63e29048df33cb6ec7c49d7bad91a82375
Akash-Sharma-2002 <akashsharma21221@gmail.com>
2025-10-28 23:51:34 +0530

    Footer.jsx: initiated

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+            voluptatem enim ad ex omnis recusandae laudantium officia, eligendi
+            beatae facilis?
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-215-456-7890</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright 2024 @Tomato.com - All Right Rerserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

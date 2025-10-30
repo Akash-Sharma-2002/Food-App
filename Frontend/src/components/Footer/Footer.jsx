83fdd5ac55daa8f5b1ef967d3d5efb4987cf49f4
Akash-Sharma-2002 <akashsharma21221@gmail.com>
2025-10-30 11:59:54 +0530

    Footer.jsx: Styling added

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -20,24 +20,26 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-content-center">
-            <h2>COMPANY</h2>
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy Policy</li>
-            </ul>
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
         </div>
         <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+1-215-456-7890</li>
-                <li>contact@tomato.com</li>
-            </ul>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-215-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
         </div>
-        <hr />
-        <p className="footer-copyright">Copyright 2024 @Tomato.com - All Right Rerserved.</p>
       </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @Tomato.com - All Right Rerserved.
+      </p>
     </div>
   );
 };
